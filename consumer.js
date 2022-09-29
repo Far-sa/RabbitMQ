83f8a8602900973a013b97a31f5b4c68d9e1e8dc
Far-sa <fmohtasham82@gmail.com>
2022-09-29 21:52:40 +0300

    use noACK

--- consumer.js
+++ consumer.js
@@ -3,14 +3,20 @@ const amqp = require('amqplib')
 //* Create Channel
 
 async function receiveFromProducer () {
-  const queueName = 'service1'
-  
   const connection = await amqp.connect('amqp://localhost:5672')
   const channel = await connection.createChannel()
+
+  const queueName = 'Task'
   await channel.assertQueue(queueName, { durable: true })
-  await channel.consume(queueName, msg => {
-    console.log(msg.content.toString())
-  })
+  let index = 0
+  await channel.consume(
+    queueName,
+    msg => {
+      console.log(`${index}`, msg.content.toString())
+      index++
+    },
+    { noAck: true }
+  )
 }
 
 receiveFromProducer()
--- producer.js
+++ producer.js
@@ -2,13 +2,20 @@ const amqp = require('amqplib')
 
 //* Create Channel
 
-async function connectToService1 () {
-  const queueName = 'service1'
-
+async function sendMsgToTask () {
   const connection = await amqp.connect('amqp://localhost:5672')
   const channel = await connection.createChannel()
+
+  const queueName = 'Task'
   await channel.assertQueue(queueName, { durable: true })
-  channel.sendToQueue(queueName, Buffer.from('Hey RabbitMQ'))
+  channel.sendToQueue(queueName, Buffer.from('Hey RabbitMQ'), {
+    persistent: true
+  })
+
   console.log('message was sent to Server1')
+  setInterval(() => {
+    connection.close()
+    process.exit(0)
+  }, 1000)
 }
-connectToService1()
+sendMsgToTask()
